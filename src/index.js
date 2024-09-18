4f79368b042d480492cc525f2da430ba60479135
RonanD653 <Ronan_D@Live.co.uk>
2024-09-18 15:02:14 +0100

    Intro

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ul>
+    <li>Monday</li>
+    <li>Tuesday</li>
+    <li>Wednsday</li>
+  </ul>
 );
 
 // If you want to start measuring performance in your app, pass a function
