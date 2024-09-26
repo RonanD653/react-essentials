ccdd9c376ff1abed2e6571016afab2b7f24649db
RonanD653 <Ronan_D@Live.co.uk>
2024-09-26 14:53:38 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useReducer, useState } from 'react';
 import './App.css';
 
 function App({authorized}) {
-  const [emotion, setEmotion] = useState("happy")
-  const [secondary, setSecondary] = useState("tired")
-
-
-  useEffect(() => {
-    console.log(`It's ${emotion} around here`)
-  }, [emotion])
-
-  useEffect(() => {
-    console.log(`It's ${secondary} around here`)
-  }, [secondary])
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+  )
 
   return (
     <>
-      <h1>Current emotion is {emotion} and {secondary}</h1>
-      <button onClick={() => setEmotion("Frustrated")}>Frustrated</button>
-      <button onClick={() => setSecondary("crabby")}>crabby</button>
+    <input type="checkbox" value={checked} onChange={toggle}></input>
+    <p>{checked ? "checked" : "Not checked"}</p>
     </>
   )
 }
